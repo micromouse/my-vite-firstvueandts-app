16f8bc925e37cffff8bacb678f8e55687333db00
micromouse <eggeggak@vip.qq.com>
2022-11-16 14:46:03 +0800

    add comment to configuration.d.ts

--- src/typings/configuration.d.ts
+++ src/typings/configuration.d.ts
@@ -10,10 +10,25 @@ declare interface IAppConfiguration {
  * oidc设置
  */
 declare interface IOidc {
+  /**
+   * 是否使用存储
+   */
   useStore: boolean
+  /**
+   * sso url
+   */
   authority: string
+  /**
+   * client id
+   */
   client_id: string
+  /**
+   * client secret
+   */
   client_secret: string
+  /**
+   * 认证成功重定向Url
+   */
   redirect_uri: string
   popup_redirect_uri: string
   response_type: string
