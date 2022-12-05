b1ac90c2df7f3cb8d24f9d492135b6fb6ad3cb00
micromouse <eggeggak@vip.qq.com>
2022-12-05 11:26:52 +0800

    update tableview.vue

--- src/views/example/table/TableView.vue
+++ src/views/example/table/TableView.vue
@@ -57,91 +57,25 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, onMounted, reactive, ref } from 'vue'
+import { defineComponent } from 'vue'
 import { Delete, Edit, Plus, Search } from '@element-plus/icons-vue'
-import useGlobalProperties from '@/infrustructures/hooks/useGlobalProperties'
-import { AxiosInstance } from 'axios'
-import { ITableItemUserInfo } from '@/typings/table'
-import TableItemView from './TableItemView.vue'
+import useTableDemo from '@/application/example/table/useTableDemo'
 
 //定义TableView组件
 export default defineComponent({
   setup() {
-    const globalProperties = useGlobalProperties()
-    const datas = ref<ITableItemUserInfo[]>([])
-    const pageTotal = ref(0)
-    const query = reactive({ name: '', address: '', pageIndex: 1, pageSize: 2 })
-
-    //组件已创建，执行搜索功能
-    onMounted(() => handleSearch())
-
-    //搜索
-    const handleSearch = () => {
-      globalProperties
-        .resolve<AxiosInstance>('$axios')
-        .get('/data/table.json')
-        .then((res) => {
-          datas.value = res.data.list
-          pageTotal.value = res.data.pageTotal
-        })
-    }
-
-    //处理分页
-    const handlePageChange = (index: number) => {
-      query.pageIndex = index
-      handleSearch()
-    }
-
-    //处理页行数改变
-    const handleSizeChange = (val: number) => {
-      query.pageSize = val
-      handleSearch()
-    }
-
-    //编辑用户信息行
-    const handleEdit = (index: number, row: ITableItemUserInfo) => {
-      globalProperties.resolveGDialog().show(TableItemView, {
-        title: '编辑用户',
-        footer: {
-          showOk: true
-        },
-        additions: {
-          userInfo: { ...row },
-          updateUserInfo: (userInfo: ITableItemUserInfo) => {
-            datas.value[index].name = userInfo.name
-            datas.value[index].address = userInfo.address
-            datas.value[index].state = userInfo.state
-          }
-        }
-      })
-    }
-
-    //删除用户信息行
-    const handleDelete = (index: number) => {
-      if (confirm('是否真的要删除当前行')) {
-        datas.value.splice(index, 1)
-      }
-    }
-
-    //新增用户信息
-    const handleAdd = () => {
-      globalProperties.resolveGDialog().show(TableItemView, {
-        title: '新增用户',
-        footer: {
-          showOk: true,
-          OkTitle: 'Save'
-        },
-        additions: {
-          userInfo: <ITableItemUserInfo>{ state: '失败' },
-          updateUserInfo: (userInfo: ITableItemUserInfo) => {
-            datas.value = datas.value.concat(userInfo)
-          }
-        }
-      })
-    }
-
-    //获得assets文件夹下图片
-    const getAssetsImage = (name: string) => new URL(`/src/assets/${name}.png`, import.meta.url).href
+    const {
+      datas,
+      pageTotal,
+      query,
+      handleSearch,
+      handlePageChange,
+      handleSizeChange,
+      handleEdit,
+      handleDelete,
+      handleAdd,
+      getAssetsImage
+    } = useTableDemo()
 
     //数据
     return {
