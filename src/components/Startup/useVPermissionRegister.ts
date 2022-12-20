ee8bf348fac45076a417abd41e2a48b505652596
micromouse <eggeggak@vip.qq.com>
2022-12-20 17:10:36 +0800

    update usevpermissionregister.ts

--- src/components/Startup/useVPermissionRegister.ts
+++ src/components/Startup/useVPermissionRegister.ts
@@ -11,11 +11,11 @@ export default function install(app: App<any>) {
       //获取当前的this实例,路由信息
       const code = binding.value || ''
       const vm: any = binding.instance
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const route = vm.$route
 
       //根据路由信息获取菜单权限信息,可获取到当前菜单的所有信息
       if (code == 'table-add') {
-        console.log('about route', route)
         el['hidden'] = true
         el.style.display = 'none'
       }
