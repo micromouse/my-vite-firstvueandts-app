42e656f4f6ab16a04cd8c1ea485e5ffe5e5c869b
micromouse <eggeggak@vip.qq.com>
2022-12-20 11:48:21 +0800

    update editorv3view.vue

--- src/views/example/Editorv3View.vue
+++ src/views/example/Editorv3View.vue
@@ -29,7 +29,11 @@ export default defineComponent({
     Toolbar
   },
   setup() {
-    // 编辑器实例，必须用 shallowRef
+    /**
+     * 编辑器实例，必须用 shallowRef
+     * 如果有一个对象数据，结构比较深, 但变化时只是外层属性变化 ===> shallowReactive。
+     * 如果有一个对象数据，后续功能不会修改该对象中的属性，而是生新的对象来替换 ===> shallowRef。
+     */
     const editorRef = shallowRef<IDomEditor | undefined>()
     const valueHtml = ref('<p>hello</p><hr />')
     const toolbarConfig = {}
