adf3ff8280bb7096ef2e67f164cf4e3cc2f0d412
micromouse <eggeggak@vip.qq.com>
2022-12-30 14:44:20 +0800

    update menu1-3view.vue

--- src/views/nested/menu1/menu1-3/Menu1-3View.vue
+++ src/views/nested/menu1/menu1-3/Menu1-3View.vue
@@ -1,7 +1,9 @@
 <template>
-  <el-alert :closable="false" title="menu 1-3" type="info">
-    <h2>this is Menu 1-3</h2>
-  </el-alert>
+  <div style="padding: 20px">
+    <el-alert :closable="true" title="menu 1-3" type="error">
+      <h2>this is Menu 1-3</h2>
+    </el-alert>
+  </div>
 </template>
 <script lang="ts">
 import { ElAlert } from 'element-plus'
